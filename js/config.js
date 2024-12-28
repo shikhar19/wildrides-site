a9f325c1a84df9fb6e3f158895ae7d1e411c50df
shikhar19 <30972366+shikhar19@users.noreply.github.com>
2024-12-28 13:08:23 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://dc9ow3qq29.execute-api.eu-north-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
